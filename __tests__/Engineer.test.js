1eb66c472c58b6ecd26880128622478dce3b095b
magicalbeauty91 <magicalbeauty91@gmail.com>
2022-04-30 23:32:58 -0500

    added test to Intern and it passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,5 +3,5 @@ const Engineer = require('../lib/Engineer');
 test('add github to employee class', () => {
     const engineer = new Engineer('Marissa', '956', 'marissa.martinez91@outlook.com', 'magical91');
 console.log(engineer)
-    expect(engineer.github).toEqual('magical91')
+    expect(engineer.github).toEqual('magical91');
 });
